d6d336e24de6363449695b7cded41f8dacd67bcc
natalebedeva23022013@gmail.com <natalebedeva23022013@gmail.com>
2022-01-20 15:47:02 +0300

    ConvertPicture+test

---
+++ TestsWithoutLeaning/ConvertPictureTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuronLandscape;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronLandscape.Tests
+{
+    [TestClass()]
+    public class ConvertPictureTests
+    {
+        
+        [TestMethod()]
+        public void SaveTest()
+        {
+            var convertet = new ConvertPicture();
+            var ress = convertet.ConvertInPixel(@"E:\Neuron Landscape\TestsWithoutLeaning\image\Parasitized.png");
+           
+            convertet.Save("e:\\ress.png", ress);
+        }
+    }
+}
